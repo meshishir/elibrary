8a813405f679f8463722c8051407ecee64df7844
Shishir Mishra <shishirmishra100@gmail.com>
2025-06-21 09:35:46 +0545

    Initial Commit

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.get('/', (req, res) => {
   res.send('🚀 eLibrary backend is running!');
